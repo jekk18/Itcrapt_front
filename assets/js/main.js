4bed4a5674a3c26f2cf53e45047c733438f80daa
jekk18 <chelidzejeko9123741jeko@gmail.com>
2024-05-26 10:25:39 +0400

    Add burger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,17 @@ $(document).ready(function() {
         $(this).parents('.accordion-item').siblings().removeClass('active-accordion');
       });
 });
+$(document).ready(function() {
+  $('.burger-lines').click(function(){
+    $('.burger-menu').addClass('active-burger-menu');
+  });
+  $('.close-burger').click(function(){
+    $('.burger-menu').removeClass('active-burger-menu');
+  });
+});
+$(document).ready(function() {
+  $('.arrow-drop').click(function(){
+      $(this).parents('.burger-li').toggleClass('active-burger-li');
+      $(this).parents('.burger-li').siblings().removeClass('active-burger-li');
+    });
+});
